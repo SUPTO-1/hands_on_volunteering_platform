f01f5d514fdd6a719dca8f215eaaf89cb93adc89
SUPTO-1 <jahidulislamsupta9@gmail.com>
2025-03-05 21:40:49 +0600

    front-end setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-3xl text-red-500'>Hello</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/Root/Root.jsx
@@ -0,0 +1,9 @@
+const Root = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">Hello World</h2>
+        </div>
+    );
+};
+
+export default Root;
